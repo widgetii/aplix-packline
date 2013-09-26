4c40cd9bf7ca4ad7e40279e5102c56e2a510363a
Alexander Smirnov <aliaksandr.smirnou@gmail.com>
2013-09-26 14:10:23 +0300

    added wsdl to post service and junit test

---
+++ app/src/test/java/ru/aplix/packline/PostTest.java
@@ -0,0 +1,35 @@
+package ru.aplix.packline;
+
+import java.util.Map;
+
+import javax.xml.ws.BindingProvider;
+
+import junit.framework.TestCase;
+import ru.aplix.packline.post.PackingLine;
+import ru.aplix.packline.post.PackingLinePortType;
+
+public class PostTest extends TestCase {
+
+	public void testEcho() {
+		String serviceAddress = "http://z.aplix.ru/post/ws/PackingLine.1cws";
+		String userName = "4627085462743";
+		String password = "P8HLCtoOey";
+
+		PackingLine postService = new PackingLine();
+		assertNotNull(postService);
+
+		PackingLinePortType postServicePort = postService.getPackingLineSoap();
+		assertNotNull(postServicePort);
+
+		if (postServicePort instanceof BindingProvider) {
+			Map<String, Object> requestContext = ((BindingProvider) postServicePort).getRequestContext();
+			requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, serviceAddress);
+			requestContext.put(BindingProvider.USERNAME_PROPERTY, userName);
+			requestContext.put(BindingProvider.PASSWORD_PROPERTY, password);
+		}
+
+		String response = postServicePort.echo(userName);
+		assertNotNull(response);
+		assertTrue(response.indexOf(userName) >= 0);
+	}
+}
