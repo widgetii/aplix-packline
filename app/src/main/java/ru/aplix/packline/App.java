48a5e53a5a1ff3413c030c38ae4a65e70fbae8b9
Alexander Smirnov <aliaksandr.smirnou@gmail.com>
2013-11-08 11:57:24 +0300

    1) fixed app window size on Linux 2) added 2d barcode to 113 form

--- app/src/main/java/ru/aplix/packline/App.java
+++ app/src/main/java/ru/aplix/packline/App.java
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -44,11 +45,11 @@ import ru.aplix.packline.workflow.WorkflowAction;
 import ru.aplix.packline.workflow.WorkflowContext;
 import ru.aplix.packline.workflow.WorkflowController;
 
-// TODO 56 DataMatrix barcode
 // TODO 57 EZPL language, print markers
 // TODO 60 scales stabilizing event
 // TODO 90 styles of act-table, menu and quantity buttons
-// FIXME logout doesn't work on Linux
+// FIXME Byte9 scales hand up
+// FIXME logout doesn't work in ubuntu
 // FIXME error checker should hide error message after 5s even though barcode scanner is turned off
 
 public class App extends Application implements IdleListener {
@@ -67,10 +68,13 @@ public class App extends Application implements IdleListener {
 	}
 
 	@Override
-	public void init() throws FileNotFoundException, MalformedURLException, JAXBException {
-		applicationContext = new ClassPathXmlApplicationContext("/resources/spring/spring-context.xml");
+	public void init() throws FileNotFoundException, MalformedURLException,
+			JAXBException {
+		applicationContext = new ClassPathXmlApplicationContext(
+				"/resources/spring/spring-context.xml");
 
-		Configuration.setConfigFileName(getParameters().getNamed().get("config"));
+		Configuration.setConfigFileName(getParameters().getNamed()
+				.get("config"));
 		Configuration.getInstance();
 
 		shutdownExecutor = false;
@@ -78,14 +82,20 @@ public class App extends Application implements IdleListener {
 		executor = Executors.newSingleThreadScheduledExecutor();
 	}
 
-	private void initWindow(Stage stage, Rectangle2D screenBounds) {
+	private void initWindow(final Stage stage, final Rectangle2D screenBounds) {
 		stage.setTitle("Aplix Pack Line");
 		stage.initStyle(StageStyle.UNDECORATED);
 		stage.setResizable(false);
-		stage.setX(screenBounds.getMinX());
-		stage.setY(screenBounds.getMinY());
-		stage.setWidth(screenBounds.getWidth());
-		stage.setHeight(screenBounds.getHeight());
+
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				stage.setX(screenBounds.getMinX());
+				stage.setY(screenBounds.getMinY());
+				stage.setWidth(screenBounds.getWidth());
+				stage.setHeight(screenBounds.getHeight());
+			}
+		});
 	}
 
 	@Override
@@ -97,7 +107,8 @@ public class App extends Application implements IdleListener {
 		// Starting workflow
 		if (workflowContext == null) {
 			workflowContext = new StandardWorkflowContext();
-			workflowContext.setAttribute(Const.APPLICATION_CONTEXT, applicationContext);
+			workflowContext.setAttribute(Const.APPLICATION_CONTEXT,
+					applicationContext);
 			workflowContext.setAttribute(Const.STAGE, stage);
 			workflowContext.setAttribute(Const.SCREEN_BOUNDS, screenBounds);
 			workflowContext.setAttribute(Const.EXECUTOR, executor);
@@ -106,10 +117,12 @@ public class App extends Application implements IdleListener {
 		initializeHardware();
 		initializeRemoteServices();
 
-		WorkflowAction wa = (WorkflowAction) applicationContext.getBean(Const.FIRST_WORKFLOW_ACTION_BEAN_NAME);
+		WorkflowAction wa = (WorkflowAction) applicationContext
+				.getBean(Const.FIRST_WORKFLOW_ACTION_BEAN_NAME);
 		wa.execute(workflowContext);
 
-		UserActivityMonitor.setTreshold(Configuration.getInstance().getActivityMonitorConfiguration().getIdleTresholdInMillis());
+		UserActivityMonitor.setTreshold(Configuration.getInstance()
+				.getActivityMonitorConfiguration().getIdleTresholdInMillis());
 		UserActivityMonitor.getInstance().setIdleListener(this);
 		UserActivityMonitor.getInstance().setWorkflowContext(workflowContext);
 		UserActivityMonitor.getInstance().start();
@@ -120,7 +133,8 @@ public class App extends Application implements IdleListener {
 		UserActivityMonitor.getInstance().cancel();
 
 		// Terminate current action(controller) if exists
-		WorkflowController currentController = (WorkflowController) workflowContext.getAttribute(Const.CURRENT_WORKFLOW_CONTROLLER);
+		WorkflowController currentController = (WorkflowController) workflowContext
+				.getAttribute(Const.CURRENT_WORKFLOW_CONTROLLER);
 		if (currentController != null) {
 			currentController.terminate();
 		}
@@ -131,18 +145,23 @@ public class App extends Application implements IdleListener {
 
 	@Override
 	public void idle() {
-		WorkflowAction wa = (WorkflowAction) applicationContext.getBean(Const.FIRST_WORKFLOW_ACTION_BEAN_NAME);
+		WorkflowAction wa = (WorkflowAction) applicationContext
+				.getBean(Const.FIRST_WORKFLOW_ACTION_BEAN_NAME);
 		wa.execute(workflowContext);
 	}
 
-	private void initializeHardware() throws ClassNotFoundException, FileNotFoundException, MalformedURLException, JAXBException {
-		HardwareConfiguration configuration = Configuration.getInstance().getHardwareConfiguration();
+	private void initializeHardware() throws ClassNotFoundException,
+			FileNotFoundException, MalformedURLException, JAXBException {
+		HardwareConfiguration configuration = Configuration.getInstance()
+				.getHardwareConfiguration();
 
 		// Create barcode scanner instance
 		if (configuration.getBarcodeScanner().isEnabled()) {
-			final BarcodeScanner<?> bs = BarcodeScannerFactory.createInstance(configuration.getBarcodeScanner().getName());
+			final BarcodeScanner<?> bs = BarcodeScannerFactory
+					.createInstance(configuration.getBarcodeScanner().getName());
 			bs.setConnectOnDemand(true);
-			bs.setConfiguration(configuration.getBarcodeScanner().getConfiguration());
+			bs.setConfiguration(configuration.getBarcodeScanner()
+					.getConfiguration());
 			bs.addConnectionListener(new BarcodeScannerConnectionListener() {
 				@Override
 				public void onConnected() {
@@ -163,9 +182,11 @@ public class App extends Application implements IdleListener {
 
 		// Create photo camera instance
 		if (configuration.getPhotoCamera().isEnabled()) {
-			final PhotoCamera<?> pc = PhotoCameraFactory.createInstance(configuration.getPhotoCamera().getName());
+			final PhotoCamera<?> pc = PhotoCameraFactory
+					.createInstance(configuration.getPhotoCamera().getName());
 			pc.setConnectOnDemand(true);
-			pc.setConfiguration(configuration.getPhotoCamera().getConfiguration());
+			pc.setConfiguration(configuration.getPhotoCamera()
+					.getConfiguration());
 			pc.addConnectionListener(new PhotoCameraConnectionListener() {
 				@Override
 				public void onConnected() {
@@ -186,7 +207,8 @@ public class App extends Application implements IdleListener {
 
 		// Create scales instance
 		if (configuration.getScales().isEnabled()) {
-			final Scales<?> sc = ScalesFactory.createInstance(configuration.getScales().getName());
+			final Scales<?> sc = ScalesFactory.createInstance(configuration
+					.getScales().getName());
 			sc.setConnectOnDemand(true);
 			sc.setConfiguration(configuration.getScales().getConfiguration());
 			sc.addConnectionListener(new ScalesConnectionListener() {
@@ -210,13 +232,15 @@ public class App extends Application implements IdleListener {
 
 	private void deinitializeHardware() {
 		// Stop barcode scanner
-		BarcodeScanner<?> bs = (BarcodeScanner<?>) workflowContext.getAttribute(Const.BARCODE_SCANNER);
+		BarcodeScanner<?> bs = (BarcodeScanner<?>) workflowContext
+				.getAttribute(Const.BARCODE_SCANNER);
 		if (bs != null) {
 			bs.disconnect();
 		}
 
 		// Stop photo camera
-		PhotoCamera<?> pc = (PhotoCamera<?>) workflowContext.getAttribute(Const.PHOTO_CAMERA);
+		PhotoCamera<?> pc = (PhotoCamera<?>) workflowContext
+				.getAttribute(Const.PHOTO_CAMERA);
 		if (pc != null) {
 			pc.disconnect();
 		}
@@ -264,14 +288,16 @@ public class App extends Application implements IdleListener {
 						try {
 							// Execute task only if executor is still active
 							if (!shutdownExecutor) {
-								Connectable c = (Connectable) workflowContext.getAttribute(hardwareName);
+								Connectable c = (Connectable) workflowContext
+										.getAttribute(hardwareName);
 								c.connect();
 							}
 						} finally {
 							executorLock.unlock();
 						}
 					}
-				}, Configuration.getInstance().getHardwareConfiguration().getReconnectInterval(), TimeUnit.SECONDS);
+				}, Configuration.getInstance().getHardwareConfiguration()
+						.getReconnectInterval(), TimeUnit.SECONDS);
 			} catch (Exception e) {
 				LOG.error(null, e);
 			}
@@ -280,17 +306,22 @@ public class App extends Application implements IdleListener {
 		}
 	}
 
-	private void initializeRemoteServices() throws FileNotFoundException, MalformedURLException, JAXBException {
+	private void initializeRemoteServices() throws FileNotFoundException,
+			MalformedURLException, JAXBException {
 		PackingLine postService = new PackingLine();
 		PackingLinePortType postServicePort = postService.getPackingLineSoap();
 
 		if (postServicePort instanceof BindingProvider) {
 			PostService psConf = Configuration.getInstance().getPostService();
 
-			Map<String, Object> requestContext = ((BindingProvider) postServicePort).getRequestContext();
-			requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, psConf.getServiceAddress());
-			requestContext.put(BindingProvider.USERNAME_PROPERTY, psConf.getUserName());
-			requestContext.put(BindingProvider.PASSWORD_PROPERTY, psConf.getPassword());
+			Map<String, Object> requestContext = ((BindingProvider) postServicePort)
+					.getRequestContext();
+			requestContext.put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY,
+					psConf.getServiceAddress());
+			requestContext.put(BindingProvider.USERNAME_PROPERTY,
+					psConf.getUserName());
+			requestContext.put(BindingProvider.PASSWORD_PROPERTY,
+					psConf.getPassword());
 		}
 
 		workflowContext.setAttribute(Const.POST_SERVICE_PORT, postServicePort);
