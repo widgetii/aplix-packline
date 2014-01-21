1b4b92261c4f41e37c3ff6cc062410a8197f6300
Alexander Smirnov <aliaksandr.smirnou@gmail.com>
2014-01-21 13:45:39 +0300

    updated form 22

--- app/src/test/java/ru/aplix/packline/PostTestManual.java
+++ app/src/test/java/ru/aplix/packline/PostTestManual.java
@@ -1,15 +1,27 @@
 package ru.aplix.packline;
 
 import java.io.FileNotFoundException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.xml.bind.JAXBException;
 import javax.xml.ws.BindingProvider;
 
 import junit.framework.TestCase;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+
 import ru.aplix.packline.conf.Configuration;
 import ru.aplix.packline.conf.PostService;
 import ru.aplix.packline.post.Operator;
@@ -38,5 +50,23 @@ public class PostTestManual extends TestCase {
 		Operator operator = postServicePort.getOperator(Utils.getMACAddress());
 		assertNotNull(operator);
 		assertTrue(operator.getName() != null && operator.getName().length() > 0);
+	}
+
+	public void testController() throws Exception {
+		HttpPost httppost = new HttpPost("http://localhost:8080/packline/controller/add");
+
+		// Request parameters and other properties.
+		List<NameValuePair> params = new ArrayList<NameValuePair>(1);
+		params.add(new BasicNameValuePair("parcelId", "1234578"));
+		httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+
+		// Execute and get the response.
+		HttpClient httpclient = HttpClientBuilder.create().build();
+		HttpResponse response = httpclient.execute(httppost);
+
+		int responseCode = response.getStatusLine().getStatusCode();
+		if (HttpURLConnection.HTTP_OK != responseCode && HttpURLConnection.HTTP_MOVED_TEMP != responseCode) {
+			throw new Exception(response.getStatusLine().getReasonPhrase());
+		}
 	}
 }
